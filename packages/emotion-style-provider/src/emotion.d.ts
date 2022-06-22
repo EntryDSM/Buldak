c5e3bf0b477c299fcc767eb66e13a6462a0131f8
kimuichan <leerabi577@gmail.com>
2022-06-23 08:45:15 +0900

    chore(emotion-style-provider): style-provider 추가

---
+++ packages/emotion-style-provider/src/emotion.d.ts
@@ -0,0 +1,7 @@
+import '@emotion/react';
+import theme from './theme';
+
+declare module '@emotion/react' {
+    type ThemeType = typeof theme;
+    interface Theme extends ThemeType {}
+}
---
+++ packages/emotion-style-provider/src/global.ts
@@ -0,0 +1,14 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+const global = css`
+    @import url('https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap');
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+        border: 0;
+        font-family: 'noto-sans-cjk-kr', sans-serif;
+    }
+`;
