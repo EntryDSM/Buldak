5b36ec4f57cb0b64b72bd8619d8cb85f819d8ace
강석현 <tjrgus0713@gmail.com>
2022-06-30 10:17:27 +0900

    feat(teacher-main): pages에 컴퍼넌트들 import

--- apps/teacher/pages/index.tsx
+++ apps/teacher/pages/index.tsx
@@ -1,5 +1,20 @@
 import SideBar from '../components/SideBar';
+import ManageStudent from '../components/student';
+import styled from '@emotion/styled';
+import UserDetail from '../components/student/userDetail';
 
 export default function Home() {
-    return <SideBar managementType="student" />;
+    const asd = () => {};
+    return (
+        <>
+            <UserDetail />
+            <Wrapper>
+                <SideBar managementType="student" />
+                <ManageStudent />
+            </Wrapper>
+        </>
+    );
 }
+const Wrapper = styled.section`
+    display: flex;
+`;
