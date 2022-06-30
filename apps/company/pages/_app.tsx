a24c8d428a0e60b0c4a9e611af904a4e736fa9bb
justinlee05 <justinlee05@naver.com>
2022-06-30 17:14:55 +0900

    feat(company): style provider 적용

--- apps/company/pages/_app.tsx
+++ apps/company/pages/_app.tsx
@@ -1,28 +1,11 @@
 import { Global } from '@emotion/react';
 import { AppProps } from 'next/app';
-import Head from 'next/head';
-import globalStyles from '../styles/globalStyle';
+import { StyleProvider } from '../../../packages/emotion-style-provider/src/index';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <>
-            <Head>
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
-                />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
-                    rel="stylesheet"></link>
-                <link
-                    href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css"
-                    rel="stylesheet"
-                    type="text/css"></link>
-            </Head>
-            <Global styles={globalStyles}></Global>
+        <StyleProvider>
             <Component {...pageProps} />
-        </>
+        </StyleProvider>
     );
 }
