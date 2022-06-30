94ce54202ce0d91788658604dc88b58ca3c594a2
justinlee05 <justinlee05@naver.com>
2022-06-30 16:49:29 +0900

    feat(company-list): 학생 리스트 페이지 퍼블리싱

---
+++ apps/company/components/studentbox/StudentBox.tsx
@@ -0,0 +1,89 @@
+import styled from '@emotion/styled';
+
+interface Props {
+    profile_img: string;
+    tags: any;
+}
+
+function StudentBox() {
+    return (
+        <Wrapper>
+            <ImgBlock></ImgBlock>
+            <Body>
+                <h1 id="major">프론트엔드</h1>
+                <TagsWrapper></TagsWrapper>
+            </Body>
+            <Footer>
+                <div id="imgblock"></div>
+                <div id="wrapper-name">
+                    <p id="name">성이름</p>
+                    <p id="num">1234</p>
+                </div>
+            </Footer>
+        </Wrapper>
+    );
+}
+
+export default StudentBox;
+
+const Wrapper = styled.div`
+    width: 320px;
+    height: 340px;
+    background-color: #ffffff;
+    display: flex;
+    flex-direction: column;
+    box-shadow: 0px 6px 8px rgba(0, 0, 0, 0.08);
+    border-radius: 5px;
+    justify-content: space-between;
+`;
+
+const ImgBlock = styled.div`
+    width: 100%;
+    height: 160px;
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+    background-color: #d9d9d9;
+`;
+
+const Body = styled.div`
+    padding: 10px 15px 10px 15px;
+    height: 130px;
+    #major {
+        color: #0a4595;
+        font-size: 20px;
+        margin-bottom: 10px;
+    }
+`;
+const TagsWrapper = styled.div`
+
+`;
+const Footer = styled.div`
+    height: 50px;
+    width: 100%;
+    border-top: 1px solid #f0f0f0;
+    padding: 10px 15px 10px 15px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    #imgblock {
+        height: 30px;
+        width: 30px;
+        border: 1px solid black;
+        border-radius: 50px;
+        margin-right: 10px;
+    }
+    #wrapper-name {
+        display: flex;
+        flex-direction: row;
+        align-items:center;
+        #name {
+            font-size: 16px;
+            margin-right: 5px;
+            color: #343434;
+        }
+        #num {
+            font-size: 14px;
+            color: #b6b6b6;
+        }
+    }
+`;
