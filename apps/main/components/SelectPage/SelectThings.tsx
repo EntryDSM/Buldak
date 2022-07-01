d24a5c618e42722f2295625be44679b4f0810b88
songiwon <biob1121@naver.com>
2022-07-01 09:46:17 +0900

    feat(Main): SelectPage 리뷰 반영

--- apps/main/components/SelectPage/SelectThings.tsx
+++ apps/main/components/SelectPage/SelectThings.tsx
@@ -1,26 +1,22 @@
-import * as S from "./styled";
+import * as S from './styled';
 
-type SelectThingsType = {
-  onClickLoginType: any;
-  Name: string;
-  Text: any;
-};
+interface SelectThingsType {
+    onClickLoginType: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    Name: string;
+    Text: object;
+}
 
-const SelectThings: React.FC<SelectThingsType> = ({
-  onClickLoginType,
-  Name,
-  Text,
-}) => {
-  return (
-    <S.SelectThingsBox>
-      <S.ThingsPorfile />
-      <S.ThingsName>{Name}</S.ThingsName>
-      <S.ThingsText>{Text}</S.ThingsText>
-      <S.ThingsSelectButton onClick={onClickLoginType} name={Name}>
-        선택
-      </S.ThingsSelectButton>
-    </S.SelectThingsBox>
-  );
+const SelectThings: React.FC<SelectThingsType> = ({ onClickLoginType, Name, Text }) => {
+    return (
+        <S.SelectThingsBox>
+            <S.ThingsPorfile />
+            <S.ThingsName>{Name}</S.ThingsName>
+            <S.ThingsText>{Text}</S.ThingsText>
+            <S.ThingsSelectButton onClick={onClickLoginType} name={Name}>
+                선택
+            </S.ThingsSelectButton>
+        </S.SelectThingsBox>
+    );
 };
 
 export default SelectThings;
---
+++ apps/main/components/SelectPage/constatnt.tsx
@@ -0,0 +1,23 @@
+export const Student = (
+    <>
+        DSM 학생이시라면
+        <br />
+        학생 로그인을 이용해주세요
+    </>
+);
+ 
+export const Teacher = (
+    <>
+        선생님이시라면
+        <br />
+        선생님 로그인을 이용해주세요
+    </>
+);
+
+export const Company = (
+    <>
+        기업이시라면
+        <br />
+        기업 로그인을 이용해주세요
+    </>
+);
