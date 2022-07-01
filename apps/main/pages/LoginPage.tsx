220230faa302e0f5fa39d8dae837bdbd6ddc7e68
songiwon <biob1121@naver.com>
2022-07-01 09:51:10 +0900

    feat(Main): LoginPage 리뷰 반영

--- apps/main/pages/LoginPage.tsx
+++ apps/main/pages/LoginPage.tsx
@@ -1,49 +1,49 @@
-import { useState } from "react";
-import Image from "next/image";
-import BackImg from "../assets/img/BackImg.jpg";
-import * as S from "../components/LoginPage/styled";
+import { useState } from 'react';
+import Image from 'next/image';
+import BackImg from '../assets/img/BackImg.jpg';
+import * as S from '../components/LoginPage/styled';
 
 const LoginPage = () => {
-  type loginType = {
-    email: string;
-    password: string;
-    user_type: any;
-  };
+    interface loginType {
+        email: string;
+        password: string;
+        user_type: any;
+    }
 
-  const [loginState, setLoginState] = useState<loginType>({
-    email: "",
-    password: "",
-    user_type: "",
-  });
+    const [loginState, setLoginState] = useState<loginType>({
+        email: '',
+        password: '',
+        user_type: '',
+    });
 
-  const onChangeLoginState = (e: any) => {
-    const { name, value } = e.target;
-    setLoginState({ ...loginState, [name]: value });
-    console.log(loginState);
-  };
+    const onChangeLoginState = (e: any) => {
+        const { name, value } = e.target;
+        setLoginState({ ...loginState, [name]: value });
+        console.log(loginState);
+    };
 
-  return (
-    <S.LoginPageContainer>
-      <S.ImgContainer>
-        <Image src={BackImg} />
-      </S.ImgContainer>
-      <S.LoginContainer>
-        <S.LoginBox>
-          <S.LoginTitle>로그인</S.LoginTitle>
-          <S.LoginPoint />
-          <S.LoginInputLayout>
-            <S.LoginInputText>이메일</S.LoginInputText>
-            <S.LoginInput onChange={onChangeLoginState} name="email" />
-          </S.LoginInputLayout>
-          <S.LoginInputLayout>
-            <S.LoginInputText>비밀번호</S.LoginInputText>
-            <S.LoginInput onChange={onChangeLoginState} name="password" />
-          </S.LoginInputLayout>
-          <S.LoginButton>로그인</S.LoginButton>
-        </S.LoginBox>
-      </S.LoginContainer>
-    </S.LoginPageContainer>
-  );
+    return (
+        <S.LoginPageContainer>
+            <S.ImgContainer>
+                <Image src={BackImg} />
+            </S.ImgContainer>
+            <S.LoginContainer>
+                <S.LoginBox>
+                    <S.LoginTitle>로그인</S.LoginTitle>
+                    <S.LoginPoint />
+                    <S.LoginInputLayout>
+                        <S.LoginInputText>이메일</S.LoginInputText>
+                        <S.LoginInput onChange={onChangeLoginState} name="email" />
+                    </S.LoginInputLayout>
+                    <S.LoginInputLayout>
+                        <S.LoginInputText>비밀번호</S.LoginInputText>
+                        <S.LoginInput onChange={onChangeLoginState} name="password" />
+                    </S.LoginInputLayout>
+                    <S.LoginButton>로그인</S.LoginButton>
+                </S.LoginBox>
+            </S.LoginContainer>
+        </S.LoginPageContainer>
+    );
 };
 
 export default LoginPage;
