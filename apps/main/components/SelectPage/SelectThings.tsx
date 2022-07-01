e1b4480ab0fbf15f47ee7f058838a9131a40aa8f
songiwon <biob1121@naver.com>
2022-07-01 16:08:58 +0900

    feat(ui): SelectPage ui 개선

--- apps/main/components/SelectPage/SelectThings.tsx
+++ apps/main/components/SelectPage/SelectThings.tsx
@@ -1,15 +1,19 @@
+import Image from 'next/image';
 import * as S from './styled';
 
 interface SelectThingsType {
     onClickLoginType: (e: React.MouseEvent<HTMLButtonElement>) => void;
     Name: string;
     Text: object;
+    Img: string;
 }
 
-const SelectThings: React.FC<SelectThingsType> = ({ onClickLoginType, Name, Text }) => {
+const SelectThings: React.FC<SelectThingsType> = ({ onClickLoginType, Name, Text, Img }) => {
     return (
         <S.SelectThingsBox>
-            <S.ThingsPorfile />
+            <S.ThingsPorfile>
+                <Image src={Img} />
+            </S.ThingsPorfile>
             <S.ThingsName>{Name}</S.ThingsName>
             <S.ThingsText>{Text}</S.ThingsText>
             <S.ThingsSelectButton onClick={onClickLoginType} name={Name}>
