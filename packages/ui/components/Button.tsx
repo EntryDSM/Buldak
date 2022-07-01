b8d3c7361e01a8f179840c04d93c2faafd42a183
hyejun <hyejun0608@naver.com>
2022-07-01 12:05:50 +0900

    feat(ui): Button component 생성

---
+++ packages/ui/components/Button.tsx
@@ -0,0 +1,82 @@
+import styled from '@emotion/styled';
+import { FC, useState } from 'react';
+
+interface ButtonProps {
+    width: number;
+    height: number;
+    backgroundColor?: string;
+    borderColor?: string;
+    fontColor?: string;
+    content?: string;
+    image?: string;
+    hoverImage?: string;
+}
+
+const Button: FC<ButtonProps> = (props) => {
+    const { image, content, hoverImage } = props;
+    const [icon, setIcon] = useState<string>(image ? image : '');
+
+    return (
+        <ButtonWrapper
+            {...props}
+            onMouseOver={() => {
+                image && setIcon(hoverImage ? hoverImage : image);
+            }}
+            onMouseOut={() => {
+                image && setIcon(image);
+            }}>
+            {icon && image && <img src={icon} />}
+            {content && <p>{content}</p>}
+        </ButtonWrapper>
+    );
+};
+
+const ButtonWrapper = styled.button<ButtonProps>`
+    width: ${({ width }) => width}px;
+    height: ${({ height }) => height}px;
+    background-color: ${({ backgroundColor }) => (backgroundColor ? backgroundColor : '#ffffff')};
+    color: ${({ fontColor }) => fontColor};
+    border: ${({ borderColor }) => (borderColor ? `1px solid ${borderColor}` : 'none')};
+    border-radius: 3px;
+    font-size: 20px;
+    padding: 0px 14px;
+    display: flex;
+    justify-content: center;
+    gap: 8px;
+    > img {
+        width: 20px;
+        height: 20px;
+        display: block;
+        margin: auto;
+    }
+    > p {
+        line-height: ${({ height }) => height}px;
+    }
+    :hover {
+        background-color: ${({ backgroundColor, borderColor, theme }) => {
+            switch (backgroundColor) {
+                case theme.color.background:
+                    return '#DFE2E8';
+                case theme.color.skyblue:
+                    return '#5F88D9';
+            }
+            switch (borderColor) {
+                case theme.color.gray500:
+                    return theme.color.gray500;
+                case theme.color.navy:
+                    return theme.color.navy;
+                case theme.color.skyblue:
+                    return theme.color.skyblue;
+            }
+        }};
+        color: ${({ fontColor, backgroundColor, borderColor, theme }) => {
+            if (borderColor === theme.color.skyblue || borderColor === theme.color.navy)
+                return theme.color.white;
+            else if (backgroundColor === theme.color.skyblue) return '#D6D6D6';
+            else if (backgroundColor === theme.color.background) return '#5F88D9';
+            else return fontColor;
+        }};
+    }
+`;
+
+export default Button;
