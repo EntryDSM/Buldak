300bcbaec9b1cd8fb3bea3839c1cc80d2a4c49dc
강석현 <tjrgus0713@gmail.com>
2022-07-05 18:54:44 +0900

    feat(teacher): useState로 값 관리하게 추가

--- apps/teacher/components/company/AddCompany.tsx
+++ apps/teacher/components/company/AddCompany.tsx
@@ -1,6 +1,81 @@
 import styled from '@emotion/styled';
+import { Button, TextBox } from '@packages/ui';
+import { ChangeEvent, useState } from 'react';
+import { theme } from '@packages/emotion-style-provider/src/theme';
+
+type keyValue =
+    | 'profile_image_path'
+    | 'company_name'
+    | 'location'
+    | 'start_at'
+    | 'end_at'
+    | 'name'
+    | 'phone_number'
+    | 'email';
+
+interface CompanyInfo {
+    profile_image_path: string;
+    company_name: string;
+    location: string;
+    start_at: string;
+    end_at: string;
+    name: string;
+    phone_number: string;
+    email: string;
+}
+
+interface InputArray {
+    title: string;
+    name: keyValue;
+    placeholder: string;
+}
+
+const inputArray: InputArray[] = [
+    {
+        title: '담당자 이름',
+        name: 'name',
+        placeholder: '담당자 이름을 입력해 주세요',
+    },
+    {
+        title: '담당자 이메일',
+        name: 'email',
+        placeholder: '담당자 이메일을 입력해 주세요',
+    },
+    {
+        title: '담당자 연락처',
+        name: 'phone_number',
+        placeholder: '담당자 연락처를 입력해 주세요',
+    },
+    {
+        title: '기업 이름',
+        name: 'company_name',
+        placeholder: '기업 이름을 입력해 주세요',
+    },
+    {
+        title: '기업 주소',
+        name: 'location',
+        placeholder: '기업 주소를 입력해 주세요',
+    },
+];
 
 function AddCompany() {
+    const [companyInfo, setCompanyInfo] = useState<CompanyInfo>({
+        profile_image_path: '',
+        company_name: '',
+        location: '',
+        start_at: '',
+        end_at: '',
+        name: '',
+        phone_number: '',
+        email: '',
+    });
+    const onChangeInputValue = (e: ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.name);
+        setCompanyInfo({
+            ...companyInfo,
+            [e.target.name]: e.target.value,
+        });
+    };
     return (
         <_Wrapper>
             <_Center>
@@ -8,26 +83,33 @@ function AddCompany() {
                     <div id="img"></div>
                     <p>프로필 사진 설정</p>
                 </_ImgWrapper>
-                <Inputs title="기업 이름" placeholder="기업 이름을 입력해 주세요" />
-                <Inputs title="기업 주소" placeholder="기업 주소를 입력해 주세요" />
+                {inputArray.map((item) => {
+                    return (
+                        <_InputWrapper>
+                            <p>{item.title}</p>
+                            <TextBox
+                                width={380}
+                                type="text"
+                                name={item.name}
+                                correct={true}
+                                placeholder={item.placeholder}
+                                value={companyInfo[item.name]}
+                                onChange={onChangeInputValue}
+                            />
+                        </_InputWrapper>
+                    );
+                })}
                 <_TempCalendar />
-                <_AddButton>기업 추가</_AddButton>
+                <Button
+                    width={530}
+                    height={44}
+                    borderWidth={2}
+                    borderColor={theme.color.skyblue}
+                    fontColor={theme.color.skyblue}
+                    content="기업 추가"
+                />
             </_Center>
         </_Wrapper>
-    );
-}
-
-interface InputProps {
-    title: string;
-    placeholder: string;
-}
-
-function Inputs({ title, placeholder }: InputProps) {
-    return (
-        <_InputWrapper>
-            <p>{title}</p>
-            <input type="text" placeholder={placeholder} />
-        </_InputWrapper>
     );
 }
 
@@ -35,20 +117,19 @@ export default AddCompany;
 
 const _Wrapper = styled.div`
     width: 100%;
-    height: 100vh;
     background-color: ${({ theme }) => theme.color.white};
-    position: absolute;
     display: flex;
-    align-items: center;
-    justify-content: center;
 `;
 
 const _Center = styled.div`
+    margin: 75px auto;
     width: 530px;
-    height: 695px;
     display: flex;
     flex-direction: column;
     align-items: center;
+    > button {
+        margin-top: 25px;
+    }
 `;
 
 const _ImgWrapper = styled.div`
@@ -62,7 +143,7 @@ const _ImgWrapper = styled.div`
         width: 90px;
         height: 90px;
         border-radius: 100px;
-        border: 1px solid black;
+        border: 1px dashed black;
     }
     > p {
         font-weight: 400;
@@ -82,6 +163,7 @@ const _InputWrapper = styled.div`
     justify-content: space-between;
     margin-top: 35px;
     > p {
+        color: ${({ theme }) => theme.color.black};
         font-weight: 400;
         font-size: 22px;
         line-height: 28px;
