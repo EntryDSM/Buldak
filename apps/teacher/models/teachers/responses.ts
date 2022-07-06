e732c55bd00514b702b8776d26594ec601a4d937
강석현 <tjrgus0713@gmail.com>
2022-07-06 17:18:38 +0900

    docs(teacher): teacher api model 정의

---
+++ apps/teacher/models/teachers/responses.ts
@@ -0,0 +1,38 @@
+export interface StudentInfo {
+    student_id: string;
+    name: string;
+    gcn: number;
+    profile_image_path: string;
+    feedback_status: boolean;
+    public_status: boolean;
+    is_submitted: boolean;
+}
+export interface GetStudentListResponse {
+    student_list: StudentInfo[];
+}
+export interface GetStudentDetailResponse {
+    name: string;
+    gcn: number;
+    email: string;
+    phone_number: string;
+    major: string;
+    tag: string[];
+}
+export interface CompanyInfo {
+    company_id: number;
+    profile_image_path: string;
+    company_name: string;
+    email: string;
+}
+export interface SearchCompanyResponse {
+    company_list: CompanyInfo[];
+}
+export interface GetCompanyDetailResponse extends CompanyInfo {
+    location: string;
+    name: string;
+    phone_number: string;
+}
+export interface CreateCompanyResponse {
+    id: string;
+    password: string;
+}
