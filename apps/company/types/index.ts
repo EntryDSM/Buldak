47bf3b938841c315381a251dcad9a08c439d3d1d
justinlee05 <justinlee05@naver.com>
2022-07-11 15:07:39 +0900

    feat(company): type 추가 선언

--- apps/company/types/index.ts
+++ apps/company/types/index.ts
@@ -1,15 +1,15 @@
+export interface EachStudentType {
+    student_id: string; //'550e8400-e29b-41d4-a716-446655440000'
+    major: string; //'프론트엔드'
+    tag_list: string[];
+    name: string; //'김의찬'
+    gcn: string; //'2106'
+    preview_image_path: string; // 'https://'
+    profile_image_path: string; // 'https://~'
+}
+
 export interface StudentsListResponseType {
-    student_list: [
-        {
-            student_id: string; //'550e8400-e29b-41d4-a716-446655440000'
-            major: string; //'프론트엔드'
-            tag_list: string[];
-            name: string; //'김의찬'
-            gcn: string; //'2106'
-            preview_image_path: string; // 'https://'
-            profile_image_path: string; // 'https://~'
-        },
-    ];
+    student_list: EachStudentType[];
 }
 
 // https://{{BASE_URL}}/companies/student/list
