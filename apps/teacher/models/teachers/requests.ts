55abf118a6fb7527e9630a3560796f98d66d11da
강석현 <tjrgus0713@gmail.com>
2022-08-18 19:21:00 +0900

    docs(teacher) : 주석 추가

--- apps/teacher/models/teachers/requests.ts
+++ apps/teacher/models/teachers/requests.ts
@@ -17,4 +17,8 @@ export interface CreateCompanyRequest extends EditCompanyInfoRequest {
 }
 export type GradeValue = '1' | '2' | '3' | null;
 export type ClassNumValue = '1' | '2' | '3' | '4' | null;
+// LOCAL이 일반
+// STAY가 대기 요청 문서
+// PROTECTED는 url로만 접근 가능
+// PUBLIC은 공개 문서
 export type DocumentStatusValue = 'LOCAL' | 'STAY' | 'PROTECTED' | 'PUBLIC' | null;
