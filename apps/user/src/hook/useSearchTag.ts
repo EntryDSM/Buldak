95b69c6586f4877cd1fcc44fea65ed8b4832ed03
kimuichan <leerabi577@gmail.com>
2022-09-13 08:56:25 +0900

    feat(user): query hook 추가

---
+++ apps/user/src/hook/useSearchTag.ts
@@ -0,0 +1,12 @@
+import { useQuery } from 'react-query';
+import { queryTagResource } from '../utils/api/userResouce';
+import { queryKey } from '../utils/queryKey';
+
+const useSearchTag = (autoComplate: string, isMajor: boolean) => {
+    const key = queryKey.tags.index(autoComplate, isMajor);
+    return useQuery(key, queryTagResource, {
+        enabled: !!autoComplate,
+    });
+};
+
+export default useSearchTag;
