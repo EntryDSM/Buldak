07f545f8367be5f174b8a21041003b3a20f8597f
justinlee05 <justinlee05@naver.com>
2022-09-13 00:56:56 +0900

    @feat/preview:

---
+++ apps/user/src/components/pageView/PageArea.tsx
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+import { useEffect, useState } from 'react';
+import { TestArr } from '../../constants/elements';
+import Header from './Header';
+import Pages from './Pages';
+
+function PageArea() {
+    const [zoom, setZoom] = useState<number>(100);
+    const [templateArr, setTemplateArr] = useState(TestArr);
+    useEffect(()=>{
+    },[])
+    const zoomUp = () => {
+        if (zoom < 200) setZoom(zoom + 10);
+    };
+    const zoomDown = () => {
+        if (zoom > 60) setZoom(zoom - 10);
+    };
+    return (
+        <Background>
+            <Header zoom={zoom} zoomUp={zoomUp} zoomDown={zoomDown} />
+            <Pages zoom={zoom} templateArr={templateArr} />
+        </Background>
+    );
+}
+
+const Background = styled.div`
+    background-color: #fbfbfb;
+    width: 100%;
+    overflow-x: scroll;
+    padding-left: 350px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export default PageArea;
