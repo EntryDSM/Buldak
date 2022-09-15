30354ec4eec4c7f23a94dee56e531002614a5f3d
justinlee05 <justinlee05@naver.com>
2022-09-15 22:25:11 +0900

    feat(user) : all again

--- apps/user/components/pageEditer/view/PageArea.tsx
+++ apps/user/components/pageEditer/view/PageArea.tsx
@@ -1,12 +1,11 @@
 import styled from '@emotion/styled';
 import { useEffect, useState } from 'react';
-import { TestArr } from '../../constants/elements';
 import Header from './Header';
 import Pages from './Pages';
 
 function PageArea() {
     const [zoom, setZoom] = useState<number>(100);
-    const [templateArr, setTemplateArr] = useState(TestArr);
+    useEffect(() => {}, []);
     const zoomUp = () => {
         if (zoom < 200) setZoom(zoom + 10);
     };
@@ -16,17 +15,16 @@ function PageArea() {
     return (
         <Background>
             <Header zoom={zoom} zoomUp={zoomUp} zoomDown={zoomDown} />
-            <Pages zoom={zoom} templateArr={templateArr} />
+            <Pages zoom={zoom} />
         </Background>
     );
 }
-
 const Background = styled.div`
     background-color: #fbfbfb;
     width: 100%;
-    min-height:100vh;
-    height:fit-content;
-    overflow-x: scroll;
+    min-height: 100vh;
+    height: fit-content;
+    overflow-x: hidden;
     padding-left: 350px;
     display: flex;
     flex-direction: column;
