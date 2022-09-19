8516d151e1f4bc01a060cb415c0f3796a91f9fc3
justinlee05 <justinlee05@naver.com>
2022-09-20 08:14:51 +0900

    cookie token 연결

--- apps/user/src/utils/api/instance.ts
+++ apps/user/src/utils/api/instance.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
+import { Cookies } from 'react-cookie';
+
+const cookie = new Cookies();
 
 export const instance = axios.create({
     baseURL: 'https://server.dsm-repo.com',
     timeout: 10000,
     headers: {
         Authorization:
-            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJlOWU2MjE1MS0wMGNiLTQ5MzItYmE1Yi1iZWY2YjVkYTAyYjIiLCJyb2xlIjoiU1RVREVOVCIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJleHAiOjE2NjM1Mzc3MzJ9.vg7ld7zhw724Kc5J_CiEAzCtgA6CQC99JmGGBUjhe8rSWN1WQiv29MMDsz6sAgqsv5Nkrt27kLKLSYkz9yyIow',
+            `Bearer ${cookie.get('access-token')}`,
     },
 });
