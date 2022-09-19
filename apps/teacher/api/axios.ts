a56158f59abcece86952ca396764a08501ffe3cc
songiwon <biob1121@naver.com>
2022-09-20 01:45:55 +0900

    fix(teacher): 병합버그 해결

--- apps/teacher/api/axios.ts
+++ apps/teacher/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: 'https://server.dsm-repo.com',
+    baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}`,
     timeout: 10000,
     headers: {
         Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjMzZjMjcwOS0wNTZhLTQ2YmEtOGQ2ZS1hZTg4N2U4ZjZkM2IiLCJyb2xlIjoiVEVBQ0hFUiIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJleHAiOjE2NjM1MzkyNjl9.EV1ti_Gh5dOkufJe7z_tfScnp4EXAHvYhv7R0hCwq3OXdcDQ_5Hu58Mh60o2UWBDakM7LDhO6lw3wop5zq943Q`,
