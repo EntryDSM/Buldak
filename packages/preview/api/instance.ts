70e5cfa8fa107774e0e6aacfc1871317813f3bf1
justinlee05 <justinlee05@naver.com>
2022-09-29 03:28:55 +0900

    fix : 토큰

--- packages/preview/api/instance.ts
+++ packages/preview/api/instance.ts
@@ -7,7 +7,6 @@ export const instance = axios.create({
     baseURL: 'https://server.dsm-repo.com',
     timeout: 10000,
     headers: {
-        Authorization:
-            `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJlOWU2MjE1MS0wMGNiLTQ5MzItYmE1Yi1iZWY2YjVkYTAyYjIiLCJyb2xlIjoiU1RVREVOVCIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJleHAiOjE2NjM1Mzc3MzJ9.vg7ld7zhw724Kc5J_CiEAzCtgA6CQC99JmGGBUjhe8rSWN1WQiv29MMDsz6sAgqsv5Nkrt27kLKLSYkz9yyIow`,
+        Authorization: `Bearer ${cookie.get('access_token')}`,
     },
 });
