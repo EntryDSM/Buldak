db4a91f4cffde325cf9e6cb7bc170aa24fc0204f
songiwon <biob1121@naver.com>
2022-09-29 16:32:09 +0900

    fix(main): 비밀번호 변경시 예외 처리

--- apps/main/pages/first-login-page/company/stage-one.tsx
+++ apps/main/pages/first-login-page/company/stage-one.tsx
@@ -33,8 +33,13 @@ const StageOne = () => {
             .then((res) => {
                 window.location.href = './stage-two';
             })
-            .catch((res) => {
-                alert('비밀번호 수정에 실패했습니다');
+            .catch((error) => {
+                console.log(error.response);
+                if (error?.response?.status == 400) {
+                    alert('비밀번호는 소문자, 숫자, 특수문자가 포함되어야 합니다.');
+                } else if (error?.response?.status == 401) {
+                    alert('토큰이 잘못 됐습니다');
+                }
                 setNewPasswordState({ ...newPasswordState, new_password: '' });
             });
     };
