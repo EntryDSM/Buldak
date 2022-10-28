496f587563e06eb648b7929ad188232a554d9b9c
Josanghyeon <BStkdgus.dev@gmail.com>
2022-10-28 16:19:29 +0900

    fix: 기업 토큰 삭제

--- apps/company/api/axios.ts
+++ apps/company/api/axios.ts
@@ -1,12 +1,6 @@
 import axios from 'axios';
-import { Cookies } from 'react-cookie';
-
-const cookie = new Cookies();
 
 export const instance = axios.create({
     baseURL: 'https://server.dsm-repo.com',
     timeout: 10000,
-    headers: {
-        Authorization: `Bearer ${cookie.get('access_token')}`,
-    },
 });
